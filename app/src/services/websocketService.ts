61b12266eb38726c261eea3d19a1c1b77da3cb2a
yashpatel20 <yashpatel20>
2024-02-26 12:01:33 -0500

    added websocket reconnect

--- app/src/services/websocketService.ts
+++ app/src/services/websocketService.ts
@@ -1,15 +1,17 @@
 class WebSocketService {
-  ws: WebSocket;
-  listeners: Array<(message: any) => void>;
-  constructor() {
-    this.ws = null;
-    this.listeners = [];
-  }
+  ws = null;
+  listeners = [];
+  url = ""; // Store the URL to reconnect
+  reconnectInterval = 5000; // Time between reconnection attempts in milliseconds
+  reconnectAttempts = 0;
+  maxReconnectAttempts = 10; // Maximum number of reconnection attempts
 
-  //TODO retry connection
+  constructor() {}
+
   connect(url) {
+    this.url = url; // Store the URL for reconnection
     if (this.ws) {
-      console.log("WebSocket is already connected.");
+      console.log("WebSocket is already connected or connecting.");
       return;
     }
 
@@ -22,6 +24,7 @@ class WebSocketService {
 
     this.ws.onopen = () => {
       console.log("WebSocket connection established");
+      this.reconnectAttempts = 0; // Reset reconnect attempts on successful connection
     };
 
     this.ws.onerror = (error) => {
@@ -31,7 +34,24 @@ class WebSocketService {
     this.ws.onclose = () => {
       console.log("WebSocket connection closed");
       this.ws = null;
+      this.tryReconnect();
     };
+  }
+
+  tryReconnect() {
+    if (this.reconnectAttempts < this.maxReconnectAttempts) {
+      console.log(
+        `Attempting to reconnect... (${this.reconnectAttempts + 1}/${
+          this.maxReconnectAttempts
+        })`
+      );
+      setTimeout(() => {
+        this.reconnectAttempts++;
+        this.connect(this.url);
+      }, this.reconnectInterval);
+    } else {
+      console.log("Max reconnection attempts reached. Giving up.");
+    }
   }
 
   addListener(listener) {
